90e5858809c42f91c5b22c990fc45c24c02866ce
Ivan Loncar <ivan.loncar2@fer.hr>
2017-03-08 11:32:22 +0100

    better calculation of anchor positions

--- localize_multi_bi_master/localize_multi_bi_master.ino
+++ localize_multi_bi_master/localize_multi_bi_master.ino
@@ -11,10 +11,18 @@ bool remote = false;                                    // set this to true to u
 
 boolean use_processing = true;                         // set this to true to output data for the processing sketch   
 
+// laser dist
+// uint8_t num_anchors = 4;                                    // the number of anchors
+// uint16_t anchors[4] = {0x601E, 0x6053, 0x6051, 0x6017};     // the network id of the anchors: change these to the network ids of your anchors.
+// int32_t anchors_x[4] = {0, -19230, 18450, -8500};               // anchor x-coorindates in mm
+// int32_t anchors_y[4] = {19400, 2560, -6000, -17436};                  // anchor y-coordinates in mm
+// int32_t heights[4] = {2070, 2800, 2850, 2250};              // anchor z-coordinates in mm
+
+// rf dist
 uint8_t num_anchors = 4;                                    // the number of anchors
 uint16_t anchors[4] = {0x601E, 0x6053, 0x6051, 0x6017};     // the network id of the anchors: change these to the network ids of your anchors.
-int32_t anchors_x[4] = {0, -19230, 18450, -8500};               // anchor x-coorindates in mm
-int32_t anchors_y[4] = {19400, 2560, -6000, -17436};                  // anchor y-coordinates in mm
+int32_t anchors_x[4] = {0, -19117, 18440, -8281};               // anchor x-coorindates in mm
+int32_t anchors_y[4] = {19268, 2407, -5587, -17397};                  // anchor y-coordinates in mm
 int32_t heights[4] = {2070, 2800, 2850, 2250};              // anchor z-coordinates in mm
 
 uint8_t dummy_byte = 0x23;
